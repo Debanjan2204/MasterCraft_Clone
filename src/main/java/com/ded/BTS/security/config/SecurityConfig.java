5492b7ee4ba8a98bdfef7e08922ad5d31bcb8ae2
Debanjan2204 <debanjan.clg@gmail.com>
2026-02-17 00:55:54 +0530

    Added OpenAPI for Swagger UI

--- src/main/java/com/ded/BTS/security/config/SecurityConfig.java
+++ src/main/java/com/ded/BTS/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ded.BTS.security.config;
 
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +19,9 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.ded.BTS.security.filters.JwtAuthenticationFilter;
 
@@ -77,5 +81,18 @@ public class SecurityConfig {
 	            )
 	        )
 	        .addSecurityItem(new SecurityRequirement().addList("oauth2"));
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+	    configuration.setAllowedOrigins(List.of("https://mastercraftclone-production.up.railway.app"));
+	    configuration.setAllowedMethods(List.of("*"));
+	    configuration.setAllowedHeaders(List.of("*"));
+	    configuration.setAllowCredentials(true);
+
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    return source;
 	}
 }
