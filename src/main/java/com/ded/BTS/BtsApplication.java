bc1cf9f2eb21f852f05a99d56078f3967acf8f8a
Debanjan2204 <debanjan.clg@gmail.com>
2026-03-07 23:59:54 +0530

    Mastercraft_Clone version 2.0.0

--- src/main/java/com/ded/BTS/BtsApplication.java
+++ src/main/java/com/ded/BTS/BtsApplication.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class BtsApplication {
 
 	public static void main(String[] args) {
+		 Dotenv dotenv = Dotenv.load();
+	        System.setProperty("PGHOST", dotenv.get("PGHOST"));
+	        System.setProperty("PGDATABASE", dotenv.get("PGDATABASE"));
+	        System.setProperty("PGUSER", dotenv.get("PGUSER"));
+	        System.setProperty("PGPASSWORD", dotenv.get("PGPASSWORD"));
+	        System.setProperty("PGSSLMODE", dotenv.get("PGSSLMODE"));
 		SpringApplication.run(BtsApplication.class, args);
 	}
 
--- target/generated-sources/annotations/com/ded/BTS/DTO/request/mapper/CreateProjectRequestMapperImpl.java
+++ target/generated-sources/annotations/com/ded/BTS/DTO/request/mapper/CreateProjectRequestMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-03-07T20:28:56+0530",
+    date = "2026-03-07T23:49:51+0530",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.41.0.v20250213-1140, environment: Java 21.0.6 (Eclipse Adoptium)"
 )
 @Component
@@ -21,9 +21,9 @@ public class CreateProjectRequestMapperImpl implements CreateProjectRequestMappe
 
         Project project = new Project();
 
-        project.setDescription( createProjectRequest.description() );
-        project.setName( createProjectRequest.name() );
         project.setProjectKey( createProjectRequest.projectKey() );
+        project.setName( createProjectRequest.name() );
+        project.setDescription( createProjectRequest.description() );
 
         return project;
     }
@@ -34,14 +34,14 @@ public class CreateProjectRequestMapperImpl implements CreateProjectRequestMappe
             return;
         }
 
-        if ( createProjectRequest.description() != null ) {
-            project.setDescription( createProjectRequest.description() );
+        if ( createProjectRequest.projectKey() != null ) {
+            project.setProjectKey( createProjectRequest.projectKey() );
         }
         if ( createProjectRequest.name() != null ) {
             project.setName( createProjectRequest.name() );
         }
-        if ( createProjectRequest.projectKey() != null ) {
-            project.setProjectKey( createProjectRequest.projectKey() );
+        if ( createProjectRequest.description() != null ) {
+            project.setDescription( createProjectRequest.description() );
         }
 
         project.setUpdatedAt( java.time.Instant.now() );
